a36105ceb2cf5f2a641fb2019886fa62cdb6d94f
xinyi127 <1224179212@qq.com>
2023-09-01 22:11:10 +0800

    修改 bug

--- TinySTL/functional.h
+++ TinySTL/functional.h
@@ -200,69 +200,65 @@ namespace mystl
         };
 
     // 对于整型类型，只是返回原值
-#define MYSTL_TRIVIAL_HASH_FCN(Type)         
-
-    template <> struct hash<Type>                
-
-    {                                            
-
-        size_t operator()(Type val) const noexcept 
-
-        { return static_cast<size_t>(val); }       
+#define MYSTL_TRIVIAL_HASH_FCN(Type)
+    template <> struct hash<Type>
+    {
+        size_t operator()(Type val) const noexcept
+        { return static_cast<size_t>(val); }
 
     };
 
     MYSTL_TRIVIAL_HASH_FCN(bool)
 
-        MYSTL_TRIVIAL_HASH_FCN(char)
+    MYSTL_TRIVIAL_HASH_FCN(char)
 
-        MYSTL_TRIVIAL_HASH_FCN(signed char)
+    MYSTL_TRIVIAL_HASH_FCN(signed char)
 
-        MYSTL_TRIVIAL_HASH_FCN(unsigned char)
+    MYSTL_TRIVIAL_HASH_FCN(unsigned char)
 
-        MYSTL_TRIVIAL_HASH_FCN(wchar_t)
+    MYSTL_TRIVIAL_HASH_FCN(wchar_t)
 
-        MYSTL_TRIVIAL_HASH_FCN(char16_t)
+    MYSTL_TRIVIAL_HASH_FCN(char16_t)
 
-        MYSTL_TRIVIAL_HASH_FCN(char32_t)
+    MYSTL_TRIVIAL_HASH_FCN(char32_t)
 
-        MYSTL_TRIVIAL_HASH_FCN(short)
+    MYSTL_TRIVIAL_HASH_FCN(short)
 
-        MYSTL_TRIVIAL_HASH_FCN(unsigned short)
+    MYSTL_TRIVIAL_HASH_FCN(unsigned short)
 
-        MYSTL_TRIVIAL_HASH_FCN(int)
+    MYSTL_TRIVIAL_HASH_FCN(int)
 
-        MYSTL_TRIVIAL_HASH_FCN(unsigned int)
+    MYSTL_TRIVIAL_HASH_FCN(unsigned int)
 
-        MYSTL_TRIVIAL_HASH_FCN(long)
+    MYSTL_TRIVIAL_HASH_FCN(long)
 
-        MYSTL_TRIVIAL_HASH_FCN(unsigned long)
+    MYSTL_TRIVIAL_HASH_FCN(unsigned long)
 
-        MYSTL_TRIVIAL_HASH_FCN(long long)
+    MYSTL_TRIVIAL_HASH_FCN(long long)
 
-        MYSTL_TRIVIAL_HASH_FCN(unsigned long long)
+    MYSTL_TRIVIAL_HASH_FCN(unsigned long long)
 
 #undef MYSTL_TRIVIAL_HASH_FCN
 
-        // 对于浮点数，逐位哈希
-        inline size_t bitwise_hash(const unsigned char* first, size_t count)
-        {
-            // 哈希函数使用 FNV-1 hash 算法
+    // 对于浮点数，逐位哈希
+    inline size_t bitwise_hash(const unsigned char* first, size_t count)
+    {
+        // 哈希函数使用 FNV-1 hash 算法
 #if (_MSC_VER && _WIN64) || ((GNUC || clang) &&SIZEOF_POINTER == 8)
-            const size_t fnv_offset = 14695981039346656037ull;
-            const size_t fnv_prime = 1099511628211ull;
+        const size_t fnv_offset = 14695981039346656037ull;
+        const size_t fnv_prime = 1099511628211ull;
 #else
-            const size_t fnv_offset = 2166136261u;
-            const size_t fnv_prime = 16777619u;
+        const size_t fnv_offset = 2166136261u;
+        const size_t fnv_prime = 16777619u;
 #endif
-            size_t result = fnv_offset;
-            for (size_t i = 0; i < count; ++i)
-            {
-                result ^= static_cast<size_t>(first[i]);
-                result *= fnv_prime;
-            }
-            return result;
+        size_t result = fnv_offset;
+        for (size_t i = 0; i < count; ++i)
+        {
+            result ^= static_cast<size_t>(first[i]);
+            result *= fnv_prime;
         }
+        return result;
+    }
 
     // 浮点数类型的哈希函数偏特化版本
     template <>
